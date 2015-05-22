f01c3d860544c0976ebcde771dd4d3f1fa928c2b
Joannadelossantos@outlook.com <joannadelossantos@Joannas-MacBook-Pro.local>
2015-05-22 12:49:43 -0400

    Desktop and mobile dropdown working

--- js/menu-aim.js
+++ js/menu-aim.js
@@ -1,89 +1,95 @@
-var $menu = $(".dropdown-menu");
+var flag;
 
-$menu.menuAim({
-    activate: activatemenuactive,
-    deactivate: deactivatemenuactive
-});
+$(window).resize(function() {
+    var bodyWidth = $(window).width();
+    if (flag !== false && bodyWidth < 767) {
+        flag = false;
 
-function activatemenuactive(row) {
-    var $row = $(row);
-    $row.find(".mega-menu-dropdown").addClass("menu-active");
-}
+        $('#home').click(function () {
+            window.location.replace("http://localhost:8888/megamenu");
+        });
+        //$('#e-i').click(function () {
+        //    $('#employee-information').addClass('menu-active')
+        //
+        //    if ($('#employee-information').hasClass('menu-active')){
+        //        $('#e-i').click(function () {
+        //            $('#employee-information').removeClass('menu-active')
+        //        });
+        //    }
+        //
+        //    $('#employee-information').addClass('menu-active')
+        //    // remove these classes if they are active
+        //    $('#clinical-information').removeClass('menu-active')
+        //    $('#departments').removeClass('menu-active')
+        //    $('#applications').removeClass('menu-active')
+        //    $('#directories').removeClass('menu-active')
+        //
+        //});
+        //$('#c-i').click(function () {
+        //    $('#clinical-information').addClass('menu-active')
+        //    // remove these classes if they are active
+        //    $('#employee-information').removeClass('menu-active')
+        //    $('#departments').removeClass('menu-active')
+        //    $('#applications').removeClass('menu-active')
+        //    $('#directories').removeClass('menu-active')
+        //});
+        //$('#depts').click(function () {
+        //    $('#departments').addClass('menu-active')
+        //    // remove these classes if they are active
+        //    $('#clinical-information').removeClass('menu-active')
+        //    $('#employee-information').removeClass('menu-active')
+        //    $('#applications').removeClass('menu-active')
+        //    $('#directories').removeClass('menu-active')
+        //});
+        //$('#apps').click(function () {
+        //    $('#applications').addClass('menu-active')
+        //    // remove these classes if they are active
+        //    $('#clinical-information').removeClass('menu-active')
+        //    $('#employee-information').removeClass('menu-active')
+        //    $('#directories').removeClass('menu-active')
+        //    $('#departments').removeClass('menu-active')
+        //
+        //});
+        //$('#dir').click(function () {
+        //    $('#directories').addClass('menu-active')
+        //    // remove these classes if they are active
+        //    $('#clinical-information').removeClass('menu-active')
+        //    $('#employee-information').removeClass('menu-active')
+        //    $('#applications').removeClass('menu-active')
+        //    $('#departments').removeClass('menu-active')
+        //});
+        $('li').click(function(e){
+            if( $(this).find('>ul').hasClass('menu-active') ){
+                $(this).children('ul').removeClass('menu-active').children('li').slideUp();
+                e.stopPropagation();
+            }
+            else{
+                $(this).children('ul').addClass('menu-active').children('li').slideDown();
+                e.stopPropagation();
+            }
+        });
+    } else if (flag !== true && bodyWidth >= 767) {
+        flag = true;
 
-function deactivatemenuactive(row) {
-    var $row = $(row);
-    $row.find(".mega-menu-dropdown").removeClass("menu-active");
-}
+        var $menu = $(".dropdown-menu");
 
-//$(window).resize(function() {
-//    if ($(this).width() > 767) {
-//
-//        var $menu = $(".dropdown-menu");
-//
-//        $menu.menuAim({
-//            activate: activatemenuactive,
-//            deactivate: deactivatemenuactive
-//            });
-//
-//        function activatemenuactive(row) {
-//            var $row = $(row);
-//            $row.find(".mega-menu-dropdown").addClass("menu-active");
-//        }
-//
-//        function deactivatemenuactive(row) {
-//            var $row = $(row);
-//            $row.find(".mega-menu-dropdown").removeClass("menu-active");
-//        }
-//
-//    }
-//    else {
-//        $('#home').click(function(){
-//
-//        });
-//        $('#e-i').click(function(){
-//            $('#employee-information').addClass('menu-active')
-//            // remove these classes if they are active
-//            $('#clinical-information').removeClass('menu-active')
-//            $('#departments').removeClass('menu-active')
-//            $('#applications').removeClass('menu-active')
-//            $('#directories').removeClass('menu-active')
-//        });
-//        $('#c-i').click(function(){
-//            $('#clinical-information').addClass('menu-active')
-//            // remove these classes if they are active
-//            $('#employee-information').removeClass('menu-active')
-//            $('#departments').removeClass('menu-active')
-//            $('#applications').removeClass('menu-active')
-//            $('#directories').removeClass('menu-active')
-//        });
-//        $('#depts').click(function(){
-//            $('#departments').addClass('menu-active')
-//            // remove these classes if they are active
-//            $('#clinical-information').removeClass('menu-active')
-//            $('#employee-information').removeClass('menu-active')
-//            $('#applications').removeClass('menu-active')
-//            $('#directories').removeClass('menu-active')
-//        });
-//        $('#apps').click(function(){
-//            $('#applications').addClass('menu-active')
-//            // remove these classes if they are active
-//            $('#clinical-information').removeClass('menu-active')
-//            $('#employee-information').removeClass('menu-active')
-//            $('#directories').removeClass('menu-active')
-//            $('#departments').removeClass('menu-active')
-//
-//        });
-//        $('#dir').click(function(){
-//            $('#directories').addClass('menu-active')
-//            // remove these classes if they are active
-//            $('#clinical-information').removeClass('menu-active')
-//            $('#employee-information').removeClass('menu-active')
-//            $('#applications').removeClass('menu-active')
-//            $('#departments').removeClass('menu-active')
-//        });
-//    }
-//});
-//
+        $menu.menuAim({
+            activate: activatemenuactive,
+            deactivate: deactivatemenuactive
+        });
+
+        function activatemenuactive(row) {
+            var $row = $(row);
+            $row.find(".mega-menu-dropdown").addClass("menu-active");
+        }
+
+        function deactivatemenuactive(row) {
+            var $row = $(row);
+            $row.find(".mega-menu-dropdown").removeClass("menu-active");
+        }
+    }
+}).resize();
+
 
 $(".dropdown-menu li").click(function(e) {
     e.stopPropagation();
